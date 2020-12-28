4a70d7487c4f0e4bf6fd1f93b040cef57462effc
DevKreo <31585723+DevKreo@users.noreply.github.com>
2020-12-28 16:07:41 +0300

    Test solution seeder

--- database/seeders/DictionaryTypeSeeder.php
+++ database/seeders/DictionaryTypeSeeder.php
@@ -20,20 +20,21 @@ class DictionaryTypeSeeder extends Seeder
         //     "dict_type_name"=>"Регионы"
         // ]);
 
-        $dict_type_names = ['dict_type_name'=>'Группы рубежей',
-        'dict_type_name'=>'Регионы',
-        'dict_type_name'=>'Тип ошибки',
-        'dict_type_name'=>'Состояние запроса',
-        'dict_type_name'=> 'Статус обращения',
-        'dict_type_name'=> 'Тип операции',
-        'dict_type_name'=> 'Привилегии',
-        'dict_type_name'=>'Производитель устройств',
-        'dict_type_name'=>'Тип устройства',
-        'dict_type_name'=>'Группы устройств',
-        'dict_type_name'=>'Регулярность',
-        'dict_type_name'=>'Параметры',
-        'dict_type_name'=>'Период запрашиваемого отчета'
-        ];
-        DB::table('dictionaries_types')->insert(array($dict_type_names));
+        $dict_type_names =[['id'=>1,'dict_type_name'=>'Группы рубежей'],
+        ['id'=>2,'dict_type_name'=>'Регионы'],
+        ['id'=>3,'dict_type_name'=>'Тип ошибки'],
+        ['id'=>4,'dict_type_name'=>'Состояние запроса'],
+        ['id'=>5, 'dict_type_name'=> 'Статус обращения'],
+        ['id'=>6,'dict_type_name'=> 'Тип операции'],
+        ['id'=>7,'dict_type_name'=> 'Привилегии'],
+        ['id'=>8, 'dict_type_name'=>'Производитель устройств'],
+        ['id'=>9,'dict_type_name'=>'Тип устройства'],
+        ['id'=>10,'dict_type_name'=>'Группы устройств'],
+        ['id'=>11,'dict_type_name'=>'Регулярность'],
+        ['id'=>12, 'dict_type_name'=>'Параметры'],
+        ['id'=>13, 'dict_type_name'=>'Период запрашиваемого отчета'
+        ]];
+        DB::table('dictionaries_types')->insert($dict_type_names);
+
     }
 }
