03584486e59332b2afcee2b05d51bc2cfaa378f0
DevKreo <31585723+DevKreo@users.noreply.github.com>
2020-12-29 15:17:48 +0300

    Fill the dictionary table

---
+++ database/seeders/DictionarySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Dictionary;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+
+use Illuminate\Database\Seeder;
+
+class DictionarySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker= Faker::create('App\Article');
+        $Dictionaries_type_id=DB::table('dictionaries_types')->pluck('id');
+        $dict_type_names =array('Группы рубежей','Регионы','Тип ошибки','Состояние запроса','Статус обращения','Тип операции','Привилегии','Производитель устройств','Тип устройства','Группы устройств','Регулярность','Параметры','Период запрашиваемого отчета');
+        foreach($Dictionaries_type_id as $id){
+            DB::table('dictionaries')->insert([
+                'name'=>$faker->randomElement($dict_type_names),
+                'dict_type_id' =>$faker->randomElement($Dictionaries_type_id)
+            ]);
+        }
+    }
+}
