324ae7b7d8998284efb1996402ea22b8db51fc57
DevKreo <31585723+DevKreo@users.noreply.github.com>
2021-01-12 14:25:43 +0300

    Full logs seeder

--- database/seeders/LogsSeeder.php
+++ database/seeders/LogsSeeder.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Log;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class LogsSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class LogsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker= Faker::create('App\Article');
+        $devices_id = DB::table('devices')->pluck('id');
+        $users_id = DB::table('users')->pluck('id');
+        $operation_type_id = DB::table('dictionaries')->where('dict_type_id', '6')->pluck('id');
+
+        foreach($devices_id as $value){
+            Log::create([
+                'log_time'=>$faker->dateTime($startDate='-3 month', $endDate = 'now'),
+                'devices_id'=>$value,
+                'users_id'=>$faker->randomElement($users_id),
+                'operation_type_id'=>$faker->randomElement($operation_type_id)
+            ]);
+        }
     }
 }
