2d44baca3930ad881c15976031c7d937a2e6346a
DevKreo <31585723+DevKreo@users.noreply.github.com>
2021-02-18 21:05:51 +0300

    Test-canvas

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,15 @@ require('@coreui/coreui/dist/js/coreui.bundle.min');
 window.Vue = require('vue');
 
 import router from './plugins/router'
+import VueKonva from 'vue-konva'
 
 //import App from 'resources/js/App.vue';
-Vue.component('test-table', require('./components/TestTable.vue').default);
-Vue.component('nav-side-bar', require('./components/NavSideBar.vue').default);
-Vue.component('new-sidebar', require('./components/NewSidebar.vue').default);
+//Vue.component('test-table', require('./components/TestTable.vue').default);
+//Vue.component('nav-side-bar', require('./components/NavSideBar.vue').default);
+Vue.component('new-sidebar', require('./components/TestTable.vue').default);
 
 
+Vue.use(VueKonva)
 const app=new Vue({
     el:"#app",
     router,
